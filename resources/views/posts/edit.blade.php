c330c4698b72bcee4ab9b87e3a38eff53b6aa5f1
TornikeKa <76065986+TornikeKa@users.noreply.github.com>
2021-08-19 23:38:29 +0400

    upd

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,70 @@
+@include('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">Create Post</div>
+
+                    <div class="card-body">
+                        <form method="post" action="/posts" enctype="multipart/form-data">
+                            @csrf
+
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">Title</label>
+
+                                <div class="col-md-6">
+                                    <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title"
+                                           value="{{ old('title') }}" autofocus placeholder="Enter Title">
+                                    @error('title')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
+
+                                <div class="col-md-6">
+                                    <textarea id="description" type="text" class="form-control @error('description') is-invalid @enderror" name="description"
+                                              value="{{ old('description') }}" autofocus placeholder="Enter description"></textarea>
+                                    @error('description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="description" class="col-md-4 col-form-label text-md-right">Upload Image</label>
+
+                                <label class=" btn">
+                                    <span class="form-control @error('image_path') is-invalid @enderror">
+                                        Select a Image
+                                    </span>
+                                    <input type="file" name="image_path" class="d-none">
+                                    @error('image_path')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </label>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Submit Post
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
