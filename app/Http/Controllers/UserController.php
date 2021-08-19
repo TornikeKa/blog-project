3f4acfcf48ed287794ee0380daf471350aa2e9ad
TornikeKa <76065986+TornikeKa@users.noreply.github.com>
2021-08-19 20:10:32 +0400

    upd

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function __construct() {
+        $this->middleware('auth');
+    }
+    public function show(User $user) {
+        return view('user.settings', compact('user'));
+    }
+}
