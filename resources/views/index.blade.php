de48efc3a825c15330b8bf20eb37a2c5f66c475f
TornikeKa <76065986+TornikeKa@users.noreply.github.com>
2021-08-20 23:51:50 +0400

    upd

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,22 +27,25 @@
                                 {{ session('status') }}
                             </div>
                         @endif
-
                         <a href="/posts/create" class="btn btn-info">Create Post</a>
                     </div>
                     <div class="row row-cols-1 row-cols-md-3 g-4">
                         @foreach($posts as $post)
                             <div class="col">
                                 <div class="card mt-4">
-{{--                                    <img src="{{ $post->image_path }}" class="card-img-top" alt="Sorry image couldn't be loaded">--}}
+{{--                                <img src="{{ $post->image_path }}" class="card-img-top" alt="Sorry image couldn't be loaded">--}}
                                     <div class="card-body">
                                         <a href="posts/{{ $post->id }}">
                                         <h5 class="card-title">{{ $post->title }}</h5>
                                         </a>
                                         <p class="card-text">{{ $post->description }}</p>
+                                        <a href="/posts/{{ $post->id }}/edit" class="btn btn-primary">Edit</a>
+                                        <form action="/posts/{{ $post->id }}" method="POST"> @csrf @method('delete')
+                                        <button class="btn btn-danger" type="submit">Delete</button>
+                                        </form>
                                     </div>
                                     <div class="card-footer">
-                                        <small class="text-muted">{{ $post->created_at }}</small>
+                                        <div class="text-muted">{{ $post->created_at }}</div>
                                     </div>
                                 </div>
                             </div>
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,4 +1,4 @@
-@include('layouts.app')
+@extends('layouts.app')
 
 @section('content')
     <div class="container">
@@ -8,7 +8,8 @@
                     <div class="card-header">Create Post</div>
 
                     <div class="card-body">
-                        <form method="post" action="/posts" enctype="multipart/form-data">
+                        <form method="post" action="/posts/{{$post->id}}" enctype="multipart/form-data">
+                            @method('patch')
                             @csrf
 
                             <div class="form-group row">
@@ -16,7 +17,8 @@
 
                                 <div class="col-md-6">
                                     <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title"
-                                           value="{{ old('title') }}" autofocus placeholder="Enter Title">
+                                           value="{{ old('title') ?? $post->title }}" autofocus placeholder="" autocomplete="off">
+
                                     @error('title')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -24,12 +26,14 @@
                                     @enderror
                                 </div>
                             </div>
+
                             <div class="form-group row">
                                 <label for="description" class="col-md-4 col-form-label text-md-right">Description</label>
 
                                 <div class="col-md-6">
-                                    <textarea id="description" type="text" class="form-control @error('description') is-invalid @enderror" name="description"
-                                              value="{{ old('description') }}" autofocus placeholder="Enter description"></textarea>
+                                    <textarea id="description" name="description"  class="form-control @error('description') is-invalid @enderror"
+                                              type="text" autofocus>{{ old('description') ?? $post->description }}</textarea>
+
                                     @error('description')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
